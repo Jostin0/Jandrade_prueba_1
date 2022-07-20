b010ad32462ed64eaa42da9841f3e936e2a224ce
_Ostin_ <_Ostin_@Ostin>
2022-07-20 14:04:33 -0500

    Evaluacion julio 7

---
+++ Documents/NetBeansProjects/Jandrade_prueba_1/src/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author _Ostin_
+ */
+public class Clase1 {
+    
+public static void metodo1() {
+    System.out.println("metodo 1 de clase");
+}
+public static void metodo2() {
+    System.out.println("Metodo 2 de clase"); 
+    }
+public static void metodo3() {
+    System.out.println("Metodo 3 de clase");
+    }
+public static void metodo4() {
+    System.out.println("Metodo 4 de clase");
+}
+public static void metodo5() {
+    System.out.println("Metodo 5 de clase");
+}
+
+
+
+
+
+
+public void metodo6(){
+    System.out.println("Metodo 6 de instancia");
+}
+public void metodo7(){
+    System.out.println("metodo 7 de instancia");
+    }
+public void metodo8(){
+    System.out.println("Metodo 8 de instancia");
+}    
+public void metodo9(){
+    System.out.println("Metodo 9 de insrtancia");
+}    
+public void metodo10(){
+    System.out.println("Metodo 10 de instancia");
+}    
+
+}
